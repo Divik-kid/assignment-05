0ae8448b6676288bb34076e0851965066e95d997
Tjorne71 <emtj@itu.dk>
2022-10-11 13:32:55 +0200

    refaktored code

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -61,76 +61,71 @@ namespace GildedRose
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                switch(Items[i].Name) {
+                    case "Sulfuras, Hand of Ragnaros":
+                        UpdateLegendaryQuality(Items[i]);
+                    break;
+                    case "Aged Brie":
+                        UpdateAgedQuality(Items[i]);
+                    break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        UpdateTicketsQuality(Items[i]);
+                    break;
+                    case "Conjured Mana Cake":
+                        UpdateTicketsQuality(Items[i]);
+                    break;
+                    default:
+                        UpdateDefaultQuality(Items[i]); 
+                    break;
                 }
             }
+        }
+
+        public void UpdateLegendaryQuality(Item item) {
+            // a legendary item, never has to be sold or decreases in Quality
+        }
+
+        public void UpdateAgedQuality(Item item) {
+            UpdateDefaultQuality(item, 1);
+        }
+
+        public void UpdateTicketsQuality(Item item) {
+            item.SellIn -= 1;
+            int qualityDecreaseRate = 1;
+            if(item.SellIn < 11) {
+                qualityDecreaseRate += 1;
+            }
+            if(item.SellIn < 6) {
+                qualityDecreaseRate += 1;
+            }
+            if(item.SellIn < 0) {
+                item.Quality = 0;
+                return;
+            }
+            item.Quality -= qualityDecreaseRate;
+        }
+        public void UpdateConjuredQuality(Item item) {
+            UpdateDefaultQuality(item, -2);
+        }
+
+        public void UpdateDefaultQuality(Item item, int qualityDecreaseRate = -1) {
+            // DayPassing
+            item.SellIn -= 1;
+            // The Quality of an item is never more than 50
+            if(item.Quality < 50) {
+                // Once the sell by date has passed, Quality degrades twice as fast
+                if(item.SellIn >= 0) {
+                    item.Quality += qualityDecreaseRate;
+                } else {
+                    item.Quality += qualityDecreaseRate*2;
+                }
+            } else {
+                item.Quality = 50;
+            }
+            // The Quality of an item is never negative
+            if(item.Quality < 0) {
+                item.Quality = 0;
+            } 
         }
 
     }
